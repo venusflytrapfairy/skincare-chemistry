3fb143354bcfd7ade1cc9f4e37f804e3342a253a
Anusha Asim <125938172+venusflytrapfairy@users.noreply.github.com>
2024-12-16 04:40:25 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="skincare-chemistry",
+    version="1.0.0",
+    author="Anusha Asim",
+    author_email="anushaasim21@yahoo.com",
+    description="A library for analyzing skincare ingredients.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/skincare-chemistry",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "numpy",
+    ],
+)
